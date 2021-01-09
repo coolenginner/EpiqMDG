5cd750d1a06731e62349cc36154957c72ff1dc08
Efigenia <maria@hostienda.com>
2021-01-09 12:53:31 -0400

    added plus button faq

--- public/mockups/ambassador/js/change_icon.js
+++ public/mockups/ambassador/js/change_icon.js
@@ -15,4 +15,36 @@ $(document).ready(function () {
                 .removeClass("fa-minus")
                 .addClass("fa-plus");
         });
+
+    $('#account-plus').on("click", function (){
+        $('#account-plus').addClass('hide-click');
+        $('#accordionExample').removeClass('hide-click');
+    });
+
+    $('#billing-plus').on("click", function (){
+        $('#billing-plus').addClass('hide-click');
+        $('#accordionExample2').removeClass('hide-click');
+    });
+
+    //services
+    $('#services-plus').on("click", function (){
+        $('#services-plus').addClass('hide-click');
+        $('#accordionExample3').removeClass('hide-click');
+    });
+
+     $('#how-it-works-plus').on("click", function (){
+        $('#how-it-works-plus').addClass('hide-click');
+        $('#accordionExample4').removeClass('hide-click');
+    });
+
+    $('#filler-plus').on("click", function (){
+        $('#filler-plus').addClass('hide-click');
+        $('#accordionExample5').removeClass('hide-click');
+    });
+
+    $('#diet-plus').on("click", function (){
+        $('#diet-plus').addClass('hide-click');
+        $('#accordionExample6').removeClass('hide-click');
+    });
+
 });
